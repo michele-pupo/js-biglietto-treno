2a2774c0e2c6830849e56d0269bccd60aac1a366
michele-pupo <mikypupo@hotmail.it>
2024-01-29 12:58:07 +0100

    celsius to fahrenheit

---
+++ snack-temperatura/js/script.js
@@ -0,0 +1,24 @@
+// Chiedi la temperatura all'utente in gradi Celsius
+// converti la temperatura in gradi Fahrenheit
+
+const celsius = prompt("Qual è la tua temperatura?");
+
+document.getElementById("temperature-celsius").innerHTML = celsius;
+
+// calcolo temperatura in Fahrenheit
+const fahrenheit = (((celsius * 9) / 5) + 32);
+
+if (celsius >= 36.5){
+    
+    console.log("ha la febbre");
+    
+    document.getElementById("temperature-celsius").innerHTML = `si, mi sa che ti sei beccato una bella influenza!`;
+    document.getElementById("temperature-fahrenheit").innerHTML = `${(fahrenheit)}°F`;
+
+} else {
+
+    console.log("non ha la febbre");
+    
+    document.getElementById("temperature-celsius").innerHTML = `non hai la febbre, stai benissimo!!`;
+    document.getElementById("temperature-fahrenheit").innerHTML = `${(fahrenheit)}°F`;
+}   
